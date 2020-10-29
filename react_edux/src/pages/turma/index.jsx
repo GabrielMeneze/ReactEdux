c3632ce82f2ab0430045b4cc6281449ef81e2e32
DEV-MatheusOFC <61585558+DEV-MatheusOFC@users.noreply.github.com>
2020-10-29 15:24:44 -0300

    Card Turma

--- react_edux/src/pages/turma/index.jsx
+++ react_edux/src/pages/turma/index.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
 import Menu from '../../components/menu';
 import Rodape from '../../components/rodape';
-import { url } from '../../utils/constants'
-import { useHistory } from "react-router-dom";
-import jwt_decode from "jwt-decode";
 import logoColorida from '../../assets/img/logoColorida.png';
 import { Form, Container, Button } from 'react-bootstrap';
 import './index.css';
 
 const Turma = () => {
-    let history = useHistory();
-
-    const [turma, setTurma] = useState('')
-
     return (
         <div style={{background : '#2a2a2a'}}>
             <Menu />
-            <Container className='form-height'style={{borderRadius : '10px', marginBottom : '8.1em', marginTop : '6em', color : 'white'}}>
+            <Container className='cont'style={{borderRadius : '10px', marginBottom : '8.1em', marginTop : '6em', color : 'white'}}>
             <div className="text-center">
                         <img src={logoColorida} alt="Edux" style={{ width: "128px",}} />
                     </div>
-                    <br/>
-                    <h2>Turmas</h2>
-                    <hr/>
-                    
-                    <Form.Group controlId="formBasicTurma">
-                        <Form.Control type="turma" value={Turma} onChange={event => setTurma(event.target.value)} placeholder="Insira a turma desejada" required />
-                    </Form.Group>
 
-                    <Button variant="success" type="submit">
-                        Buscar
-                    </Button>
+                    <div className ='blocos'>
+                        <div className ='caixa'>
+                            <br/>
+                            <h5 className ='title'>Desenvolvimento de Sistemas (Matutino)</h5>
+                            <hr/>
+                            <p>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Descrição</Button>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Objetivos</Button>
+                            </p>
+                        </div>
+                        <div className ='caixa'>
+                            <br/>
+                            <h5 className ='title'>Desenvolvimento de Sistemas (Vespertino)</h5>
+                            <hr/>
+                            <p>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Descrição</Button>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Objetivos</Button>                            </p>
+                        </div>
+                        <div className ='caixa'>
+                            <br/>
+                            <h5 className ='title'>Desenvolvimento de Sistemas (Noturno)</h5>
+                            <hr/>
+                            <p>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Descrição</Button>
+                            <Button href="/" style={{ marginBottom: '15px', marginLeft: '15px', marginRight: '15px', color: '#FFFFFF' }}>Objetivos</Button>                            </p>
+                        </div>
+                    </div>
+
             </Container>
             <Rodape />
         </div>
